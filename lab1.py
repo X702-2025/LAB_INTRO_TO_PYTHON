61c24a525937c0304bd08f4ed94d986c8dbd7589
X702-2025 <mohmmad201048@gmail.com>
2022-02-20 14:34:57 +0300

    answered

---
+++ lab1.py
@@ -0,0 +1,34 @@
+help('lambda')
+help('def')
+help('in')
+help('yield')
+
+# one value to multible variables
+
+from re import X
+
+
+myName = myFriendName = "Mohmmad"
+
+# multible values to multible variables
+
+myTown , hisTown = "Yanbu" , "albaha"
+
+# “x“ variable has an integer value and “y” has a float value
+
+x = int(5) 
+y = float(4.5)
+
+#implicit type conversion in new variable “z”
+
+z = "4"
+print(z)
+
+# explicit type conversion on the x variable
+
+x = str(5)
+print(x)
+
+# “name” variable has a value equals “Your Name”
+
+name = "Your Name"
